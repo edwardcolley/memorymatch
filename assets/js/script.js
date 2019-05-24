51875b51806f7d98f76d8218a64d53dad0075993
Edward Colley <edward.colley@outlook.com>
2019-05-23 18:05:17 -0700

    got a match

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,33 @@
 $(document).ready(function() {
     $('.front').on('click', handleCardClick)
 
+    var firstCardClicked = null;
+    var secondCardClicked = null;
+    var matched = null;
+
     function handleCardClick(event) {
         $(this).addClass('hidden');
+        debugger;
+        if (firstCardClicked === null) {
+            firstCardClicked = $(this)
+            siblings = firstCardClicked.siblings();
+            backCard = siblings.css('background-image');
+            console.log(backCard);
+        } else if (secondCardClicked === null) {
+            secondCardClicked = $(this)
+            siblings2 = secondCardClicked.siblings();
+            backCard2 = siblings2.css('background-image');
+            console.log(backCard2);
+            if (backCard2 === backCard) 
+            console.log('They match');
+            matched++;
+            firstCardClicked = null;
+            secondCardClicked = null;
+        }
+        
     }
+
 })
+
 
 
