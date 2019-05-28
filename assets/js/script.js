eecf706ffcdceb2c4dc348f2e8351bde06e51106
Edward Colley <edward.colley@outlook.com>
2019-05-28 09:33:04 -0700

    finished stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,57 @@ $(document).ready(function() {
     var firstCardClicked = null;
     var secondCardClicked = null;
     var matched = null;
-    var max_matches = 2;
+    var max_matches = 3;
+    var attempts = 0;
+    var games_played = null;
 
     function handleCardClick(event) {
         $(this).addClass('hidden');
         
         if (firstCardClicked === null) {
             firstCardClicked = $(this)
+            console.log('first card: ', firstCardClicked);
             siblings = firstCardClicked.siblings();
             backCard = siblings.css('background-image');
         } else if (secondCardClicked === null) {
             secondCardClicked = $(this)
+            console.log('second card: ', secondCardClicked);
             siblings2 = secondCardClicked.siblings();
             backCard2 = siblings2.css('background-image');
+            
+            displayStats();
+
+            
             
             if (backCard2 === backCard) {
             console.log('They match');
             matched++;
-            console.log(matched);
+            console.log('matched: ', matched);
             firstCardClicked = null;
             secondCardClicked = null;
             console.log(firstCardClicked, secondCardClicked)
+
+            displayStats();
+            } else {
+                attempts++;
+                console.log('attempts: ', attempts);
+                firstCardClicked = null;
+                secondCardClicked = null;
             }
             
             if (matched === max_matches) {
-               openModal();
+               games_played++;
+               console.log('games played: ', games_played);
+                openModal();
             }
            
+        }
+
+        function displayStats() {
+            $('#accuracy').text(calculateAccuracy);
+            $('#attempts').text(attempts);
+            $('#games_played').text(games_played);
+
         }
        
     }
@@ -38,7 +62,6 @@ $(document).ready(function() {
     var modal = $('#simpleModal');
     var modalBtn = $('#modalBtn');
     var closeBtn = $('.closeBtn')[0];
-    // var closeBtn = document.getElementsByClassName('closeBtn')[0];
     
 
     $(modalBtn).on('click', openModal);
@@ -58,6 +81,11 @@ $(document).ready(function() {
         }
     }
   
+    function calculateAccuracy() {
+        accuracy = matched / attempts;
+        console.log('accuracy: ', accuracy);
+        return accuracy;
+    }
     
 })
 
