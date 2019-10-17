606098573ec3fd3a1ae5339522f54caeff93f074
edwardcolley <edward.colley@outlook.com>
2019-10-17 00:37:00 +0000

    added a base timer that runs for 2 minutes and does no action

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,15 @@ var check1 = null;
 var check2 = null;
 
 function initializeApp() {
+    var twoMinutes = 60 * 2,
+        display = $('#time');
     $('.front').on('click', handleCardClick);
     $('.closeBtn').on('click', closeModal);
     $('.closeBtn2').on('click', closeModalDone);
     $('.closeWarningBtn').on('click', closeWarningModal);
     $('.resetBtn').on('click', resetGame);
     placeShuffledCards();
+    startTimer(twoMinutes, display);
 }
 
 function handleCardClick(event) {
@@ -171,4 +174,19 @@ function playRightSound() {
     ding.play();
 }
 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
